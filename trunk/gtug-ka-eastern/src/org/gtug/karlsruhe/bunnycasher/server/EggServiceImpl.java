b811df807dc2b8ec466d0230f4221c8b31e5052d
markus@myd0.de <markus@myd0.de@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 14:37:11 +0000

    Removed GreetingService

---
+++ trunk/gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycasher/server/EggServiceImpl.java
@@ -0,0 +1,20 @@
+package org.gtug.karlsruhe.bunnycasher.server;
+
+import org.gtug.karlsruhe.bunnycasher.client.EggService;
+import org.gtug.karlsruhe.bunnycasher.common.Egg;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class EggServiceImpl extends RemoteServiceServlet implements
+		EggService {
+
+	@Override
+	public void createEgg(Egg egg) {
+		
+	}
+
+}
