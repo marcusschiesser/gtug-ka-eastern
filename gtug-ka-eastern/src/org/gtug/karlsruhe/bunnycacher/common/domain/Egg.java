300c44f8c337adfe830a523403472a900a053996
markus@myd0.de <markus@myd0.de@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 16:52:29 +0000

    Persitence configuration

--- gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycacher/common/domain/Egg.java
+++ gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycacher/common/domain/Egg.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
@@ -15,7 +16,7 @@ import javax.persistence.TemporalType;
 public class Egg implements Serializable {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private double latitude;
