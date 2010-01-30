6d0f2d562a5be9e52f24717e453a8dfc6f708322
heiko.seebach <heiko.seebach@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 14:40:48 +0000

    eggservice extends RemoteService

--- gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycasher/client/EggService.java
+++ gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycasher/client/EggService.java
@@ -2,10 +2,11 @@ package org.gtug.karlsruhe.bunnycasher.client;
 
 import org.gtug.karlsruhe.bunnycasher.client.common.Egg;
 
+import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("egg")
-public interface EggService {
+public interface EggService  extends RemoteService {
 
 	void createEgg(Egg egg);
 	
