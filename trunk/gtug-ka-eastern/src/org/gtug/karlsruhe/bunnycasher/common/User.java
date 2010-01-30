aa5a5235642b8931d71f26b12662c9ff732bb344
markus@myd0.de <markus@myd0.de@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 11:49:54 +0000

    Domain model

---
+++ trunk/gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycasher/common/User.java
@@ -0,0 +1,15 @@
+package org.gtug.karlsruhe.bunnycasher.common;
+
+public class User {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
