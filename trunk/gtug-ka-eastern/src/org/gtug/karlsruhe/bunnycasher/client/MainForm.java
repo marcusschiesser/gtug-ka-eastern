34ef2d0c84ae0a38648455896be6e736171bc3d6
heiko.seebach <heiko.seebach@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 12:22:29 +0000

    erste Version UI mit Toolbar

---
+++ trunk/gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycasher/client/MainForm.java
@@ -0,0 +1,27 @@
+package org.gtug.karlsruhe.bunnycasher.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class MainForm extends DockPanel {
+	Button newEggButton;
+	HorizontalPanel toolBarPanel;
+
+	public MainForm() {
+		toolBarPanel=new HorizontalPanel ();
+		add(toolBarPanel,NORTH);
+		newEggButton =new Button("neues Ei verstecken",new ClickHandler() {			
+			@Override
+			public void onClick(ClickEvent event) {
+				new NewEggPopup().show();
+			}
+		});
+		toolBarPanel.add(newEggButton);
+	}
+}
