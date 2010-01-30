55cbc0a38bc5e55acecc3c264dba87edb397d4af
lowstack99 <lowstack99@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 12:52:49 +0000

    initial import

---
+++ gtug-ka-eastern/trunk/Classes/BunnyCacherAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  BunnyCacherAppDelegate.h
+//  BunnyCacher
+//
+//  Created by Holger Wiedemann on 30.01.10.
+//  Copyright YellowMap AG 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface BunnyCacherAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
