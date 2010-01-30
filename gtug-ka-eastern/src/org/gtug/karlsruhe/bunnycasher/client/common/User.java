0f538030a8e61d47d482be63e12998e866241172
heiko.seebach <heiko.seebach@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 14:53:26 +0000

    es geht!!!

--- gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycasher/client/common/User.java
+++ gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycasher/client/common/User.java
@@ -1,6 +1,13 @@
 package org.gtug.karlsruhe.bunnycasher.client.common;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private String id;
 	
