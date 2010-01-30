f5e6148b70786243aaf73411ad39106931d63e31
daniel@jagszent.de <daniel@jagszent.de@764dff50-0d76-11df-8457-13bc90d6e451>
2010-01-30 18:19:49 +0000

    phonegap: Geolocation.watchPosition callback gets only fired when the positions changes phonegap: Introduced Notification wrapper phonegap_mock: added mocking for more phonegap APIs

--- gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycacher/public/phonegap_mock.js
+++ gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycacher/public/phonegap_mock.js
@@ -1,41 +1,80 @@
-if (typeof navigator.geolocation == "undefined") {
-	navigator.geolocation = {
-		lastPosition : null,
-		getCurrentPosition : function(successCallback, errorCallback, options) {
-			if (typeof(successCallback) != 'function')
-				successCallback = function() {
+(function(){
+	function emptyFn(){}
+	
+	
+if (typeof window['PhoneGap'] == "undefined") {
+	
+	if (typeof navigator['geolocation'] == "undefined") {
+	
+		navigator.geolocation = {
+			lastPosition : null,
+			getCurrentPosition : function(successCallback, errorCallback, options) {
+				if (typeof(successCallback) != 'function')
+					successCallback = function() {
+				};
+	
+				navigator.geolocation.lastPosition = {
+					coords : {
+						latitude : 49.001771 + ((Math.random() - 0.5) * 0.001 ),
+						longitude : 8.38404 + ((Math.random() - 0.5) * 0.001 ),
+						altitude : 0.0,
+						accuracy : 0.0,
+						altitudeAccuracy : 0.0,
+						heading : 0.0,
+						speed : 0.0
+					},
+					timestamp : (new Date()).getTime()
 				};
 
-			navigator.geolocation.lastPosition = {
-				coords : {
-					latitude : 49.001771 + ((Math.random() - 0.5) * 0.001 ),
-					longitude : 8.38404 + ((Math.random() - 0.5) * 0.001 ),
-					altitude : 0.0,
-					accuracy : 0.0,
-					altitudeAccuracy : 0.0,
-					heading : 0.0,
-					speed : 0.0
-				},
-				timestamp : (new Date()).getTime()
+				successCallback(this.lastPosition);
+	
+			},
+			watchPosition : function(successCallback, errorCallback, options) {
+				this.getCurrentPosition(successCallback, errorCallback, options);
+				var frequency = 10000;
+				if (typeof(options) == 'object' && options.frequency) {
+					frequency = options.frequency;
+				}
+	
+				var that = this;
+				return setInterval(function() {
+							that.getCurrentPosition(successCallback, errorCallback,
+									options);
+						}, frequency);
+			},
+			clearWatch : function(watchId) {
+				clearInterval(watchId);
 			}
-
-			successCallback(this.lastPosition);
-
-		},
-		watchPosition : function(successCallback, errorCallback, options) {
-			this.getCurrentPosition(successCallback, errorCallback, options);
-			var frequency = 10000;
-			if (typeof(options) == 'object' && options.frequency)
-				frequency = options.frequency;
-
-			var that = this;
-			return setInterval(function() {
-						that.getCurrentPosition(successCallback, errorCallback,
-								options);
-					}, frequency);
-		},
-		clearWatch : function(watchId) {
-			clearInterval(watchId);
-		}
-	};
+		};
+		
+	}
+	
+	if (typeof navigator['notification'] == "undefined") {
+		navigator.notification = {
+			loadingStart: emptyFn,
+			loadingStop: emptyFn,
+			activityStart: emptyFn,
+			activityStop: emptyFn,
+			blink: emptyFn,
+			vibrate: emptyFn,
+			beep: emptyFn,
+			alert: function(message, title, buttonLabel){
+				alert(message);
+			}
+		};
+	}
+	
+	if (typeof navigator['device'] == "undefined") {
+		navigator.device = {
+			available: false,
+			model: 'Bogus',
+			version: '0.1',
+			gap: '0.1-mock',
+			gapVersion: '0.1-mock',
+			uuid: 'bogus-uuid'
+		};
+	}
+	
 }
+
+})();
--- gtug-ka-eastern/src/org/gtug/karlsruhe/phonegap/client/Geolocation.java
+++ gtug-ka-eastern/src/org/gtug/karlsruhe/phonegap/client/Geolocation.java
@@ -4,12 +4,48 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 public class Geolocation extends JavaScriptObject {
 	/** Required for Overlay types */
-	protected Geolocation() {}
-	
+	protected Geolocation() {
+	}
+
 	/** */
-	public static native int watchPosition(PositionSuccessCallback successCallback) /*-{
-		return $wnd.navigator.geolocation.watchPosition(function() {
-			successCallback.@org.gtug.karlsruhe.phonegap.client.PositionSuccessCallback::onPosition(DD)($wnd.navigator.geolocation.lastPosition.coords.latitude, $wnd.navigator.geolocation.lastPosition.coords.longitude);
+	public static native int watchPosition(
+			PositionSuccessCallback successCallback) /*-{
+		return $wnd.navigator.geolocation.watchPosition(function(position) {
+			function isPositionChanged(oldPosition, newPosition){ // TODO: refactor: move this to GWT code, not native JavaScript code
+				// http://www.movable-type.co.uk/scripts/latlong.html
+				// Spherical Law of Cosines
+				var R = 6371, // km
+					lat1 = oldPosition.coords.latitude  * Math.PI / 180, 
+					lon1 = oldPosition.coords.longitude * Math.PI / 180, 
+					lat2 = newPosition.coords.latitude  * Math.PI / 180, 
+					lon2 = newPosition.coords.longitude * Math.PI / 180; 
+				var d = Math.acos(Math.sin(lat1)*Math.sin(lat2) + 
+		          Math.cos(lat1)*Math.cos(lat2) *
+		          Math.cos(lon2-lon1)) * R;
+				return d > 0.005; // distance bigger than 5 meters
+				// TODO: fine-tune distance
+			}
+			var now = (new Date()).getTime();
+			if (now - position.timestamp > 60 * 60 * 5) {
+				// TODO: debug log -> stale position
+			} else {
+				if (!$wnd.navigator.geolocation.savedPosition || isPositionChanged($wnd.navigator.geolocation.savedPosition, position)) {
+					// poor man's deep copy
+					$wnd.navigator.geolocation.savedPosition = {
+						coords : {
+							latitude : position.coords.latitude,
+							longitude : position.coords.longitude,
+							altitude : position.coords.altitude,
+							accuracy : position.coords.accuracy,
+							altitudeAccuracy : position.coords.altitudeAccuracy,
+							heading : position.coords.heading,
+							speed : position.coords.speed
+						},
+						timestamp : position.timestamp
+					};
+					successCallback.@org.gtug.karlsruhe.phonegap.client.PositionSuccessCallback::onPosition(DD)(position.coords.latitude, position.coords.longitude);
+				}
+			}
 		});
 	}-*/;
 }
