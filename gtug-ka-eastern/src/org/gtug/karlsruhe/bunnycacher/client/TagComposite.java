6eb485d72b59fbf8bef309e07f6e6ace696ad904
daniel@jagszent.de <daniel@jagszent.de@764dff50-0d76-11df-8457-13bc90d6e451>
2010-04-01 02:32:54 +0000

    FoundEggView should display user id and tag now. I get a NullPointerException I don't get, thou...

---
+++ gtug-ka-eastern/src/org/gtug/karlsruhe/bunnycacher/client/TagComposite.java
@@ -0,0 +1,33 @@
+package org.gtug.karlsruhe.bunnycacher.client;
+
+import org.gtug.karlsruhe.bunnycacher.common.domain.TagDto;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Widget;
+
+public class TagComposite extends Composite {
+
+	private static TagCompositeUiBinder uiBinder = GWT
+			.create(TagCompositeUiBinder.class);
+
+	interface TagCompositeUiBinder extends UiBinder<Widget, TagComposite> {
+	}
+
+	@UiField
+	HasText userLabel;
+	
+	@UiField
+	HasText tagLabel;
+
+	public TagComposite(TagDto tag) {
+		initWidget(uiBinder.createAndBindUi(this));
+		userLabel.setText(tag.getUser().getGoogleId());
+		tagLabel.setText(tag.getMessage());
+	}
+
+
+}
